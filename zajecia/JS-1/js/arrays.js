df389e36d3de2c2c70436d6ae45a9e46fab53584
mszymanczuk <m.szymanczuk@hotmail.com>
2017-12-16 12:13:09 +0100

    sortowanie

--- zajecia/JS-1/js/arrays.js
+++ zajecia/JS-1/js/arrays.js
@@ -6,4 +6,30 @@ console.log(imiona);
 
 imiona[3] = 'Ola';
 imiona[2] = 'Wojtek';
+
+
+imiona.push('Miłosz');
+
 console.log(imiona);
+
+
+var ostatniElement = imiona.pop();
+
+console.log(ostatniElement);
+
+console.log(imiona);
+
+console.log(imiona.unshift('Robert'));
+
+console.log(imiona.shift('Robert'));
+
+console.log(imiona.length);
+
+//console.log(imiona.join());
+
+imiona.sort();
+var liczby = [1, 2, 0, 10, 12, 100, 55];
+liczby.sort();
+
+
+console.log(liczby);
