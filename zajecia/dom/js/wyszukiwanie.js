ae7c35c80ad2757c4116f5f120fd7040520756c1
mszymanczuk <m.szymanczuk@hotmail.com>
2017-12-17 10:24:57 +0100

    relacje między węzłami

--- zajecia/dom/js/wyszukiwanie.js
+++ zajecia/dom/js/wyszukiwanie.js
@@ -4,3 +4,20 @@ console.log(parFirst);
 
 var linki = document.getElementsByClassName('superLink')
 console.log(linki);
+
+var linkiPoTagu = document.getElementsByTagName('a');
+console.log(linkiPoTagu);
+
+var divPoID = document.querySelector('#parSecond');
+console.log(divPoID);
+
+var linkiPoSelektorze = document.querySelectorAll('.link');
+console.log(linkiPoSelektorze);
+
+linkiPoSelektorze.forEach(function(link){
+    console.log(link);
+});
+
+for(var i=0; i < linkiPoSelektorze.length; i++){
+    console.log(linkiPoSelektorze[i]);
+}
