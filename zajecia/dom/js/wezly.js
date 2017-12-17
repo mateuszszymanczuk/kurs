e221b4dc28fad50ffdc43d156823409a60d5b68d
mszymanczuk <m.szymanczuk@hotmail.com>
2017-12-17 11:08:51 +0100

    tworzenie wezłów

--- zajecia/dom/js/wezly.js
+++ zajecia/dom/js/wezly.js
@@ -1,2 +1,20 @@
 'use strict'
 
+var pierwszyDiv = document.getElementById('parFirst');
+console.log(pierwszyDiv);
+console.log(pierwszyDiv.parentNode);
+console.log(pierwszyDiv.parentElement);
+
+
+console.log(pierwszyDiv.childNodes);
+console.log(pierwszyDiv.children[1]);
+
+var pierwszyDivChild = pierwszyDiv.children[1];
+console.log(pierwszyDivChild.nextElementSibling);
+
+var childNodesDiva = pierwszyDiv.childNodes;
+childNodesDiva.forEach(function(element){
+    if(element.nodeType==1){
+        console.log(element);
+    }
+});
