bfb4139a9250090dbb2f649b72eb5d8191be9c1c
mszymanczuk <m.szymanczuk@hotmail.com>
2017-12-17 12:32:32 +0100

    Usuwanie klasy i dodawanie elementu

--- zajecia/dom/js/tworzenie-wezlow.js
+++ zajecia/dom/js/tworzenie-wezlow.js
@@ -13,3 +13,29 @@ console.log(newElement);
 
 istniejacyWezel.appendChild(newElement);
 istniejacyWezel.removeChild(newElement);
+
+
+var parFirstDiv = document.getElementById('parFirst');
+
+parFirstDiv.replaceChild(newElement, istniejacyWezel);
+
+var allLinks = document.querySelectorAll('a');
+console.log(allLinks);
+
+/*FOR*/
+//for(var i = 0; i < allLinks.length; i++){
+//    var br = document.createElement('br');
+//    allLinks[i].removeAttribute('class');
+//    allLinks[i].parentElement.insertBefore(br, allLinks[i].nextSibling);
+//}
+//    
+
+
+/*For Each - trzeba liste, a do listy potrzebne querySelector*/
+
+allLinks.forEach(function(element){
+    var br = document.createElement('br');
+    element.removeAttribute('class');
+    element.parentElement.insertBefore(br, element.nextSibling);
+    
+});
